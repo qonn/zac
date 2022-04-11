3ef6a4fd5b6bb8c23ebef54850f2d711484d8ea6
Furqan Anas <43688724+qonn@users.noreply.github.com>
2022-04-11 15:48:56 +0800

    return statement is ugly without ( ... ), added this to make it nicer.

--- src/parser/statement_return.rs
+++ src/parser/statement_return.rs
@@ -8,7 +8,14 @@ pub fn parse(ctx: &mut ParsingContext) -> ast::Return {
     ctx.eat(TokenKind::Return);
     ctx.eat_all_newlines();
 
-    let expr = expression::parse(ctx);
+    let expr = if ctx.get_curr_token().kind() == TokenKind::LParen {
+        ctx.eat(TokenKind::LParen);
+        let expr = expression::parse(ctx);
+        ctx.eat(TokenKind::RParen);
+        expr
+    } else {
+        expression::parse(ctx)
+    };
 
     let span_to = ctx.get_prev_token().span().from;
 
